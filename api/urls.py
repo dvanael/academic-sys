59a0b3e60d19892554402fe5e14fa8e58938d991
Ana Barbosa <anael.noki@gmail.com>
2025-12-03 18:07:30 -0300

    feat (api): adiciona views e urls de Enrollment

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 from . import viewsets
+from . import views
 
 router = routers.DefaultRouter()
 router.register(r"students", viewsets.StudentViewSet, "student")
@@ -9,4 +10,9 @@ router.register(r"courses", viewsets.CourseViewSet, "course")
 
 urlpatterns = [
     path(r"", include(router.urls)),
+    path("enrollments/create/", views.EnrollmentCreateView.as_view()),
+    path("enrollments/<int:enrollment_id>/pay/", views.EnrollmentPayView.as_view()),
+    path(
+        "students/<int:student_id>/enrollments/", views.StudentEnrollmentsView.as_view()
+    ),
 ]
---
+++ api/views.py
@@ -0,0 +1,32 @@
+from rest_framework import views
+from rest_framework.response import Response
+from .serializers import EnrollmentSerializer
+from data.models import Enrollment
+
+
+class EnrollmentCreateView(views.APIView):
+    def post(self, request):
+        serializer = EnrollmentSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=201)
+
+
+class StudentEnrollmentsView(views.APIView):
+    def get(self, request, student_id):
+        enrollments = Enrollment.objects.filter(student_id=student_id)
+        serializer = EnrollmentSerializer(enrollments, many=True)
+        return Response(serializer.data)
+
+
+class EnrollmentPayView(views.APIView):
+    def post(self, request, enrollment_id):
+        try:
+            enrollment = Enrollment.objects.get(id=enrollment_id)
+        except Enrollment.DoesNotExist:
+            return Response({"error": "Enrollment not found."}, status=404)
+
+        enrollment.status = True  # Pago
+        enrollment.save()
+        serializer = EnrollmentSerializer(enrollment, many=False)
+        return Response(serializer.data)
