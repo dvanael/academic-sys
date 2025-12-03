8cca1243c2c3c9c5fcd411ff6de0903199467597
Ana Barbosa <anael.noki@gmail.com>
2025-12-03 18:05:59 -0300

    style: exclui import não utilizados

--- api/viewsets.py
+++ api/viewsets.py
@@ -1,6 +1,4 @@
-from rest_framework import viewsets, views
-from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework import viewsets
 from .serializers import StudentSerializer, CourseSerializer
 from data.models import Student, Course
 
