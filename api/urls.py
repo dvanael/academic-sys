ea3584f504983aa2980e0f0a15a0e3fff132db7f
Ana Barbosa <anael.noki@gmail.com>
2025-12-03 11:41:36 -0300

    feat (api): adiciona urls da API

---
+++ api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework import routers
+from . import viewsets
+
+router = routers.DefaultRouter()
+router.register(r"students", viewsets.StudentViewSet, "student")
+
+
+urlpatterns = [
+    path(r"", include(router.urls)),
+]
