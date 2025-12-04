be505ad405d2f32b9f4a0ccca0bac3624dc0eea0
Ana Barbosa <anael.noki@gmail.com>
2025-12-04 17:07:53 -0300

    feat (data): adiciona models ao admin

--- data/admin.py
+++ data/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from data.models import Course, Student, Enrollment
 
 # Register your models here.
+admin.site.register(Course)
+admin.site.register(Student)
+admin.site.register(Enrollment)
