5a059d51baf6c6d059d432031af264acd54342e9
Ana Barbosa <anael.noki@gmail.com>
2025-12-05 22:12:06 -0300

    feat (client): adiciona lógica para listagem de pagamentos

--- client/views/enrollment.py
+++ client/views/enrollment.py
@@ -1,6 +1,6 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, TemplateView
-from data.models import Enrollment
+from data.models import Enrollment, Course, Student
 from typing import Any
 from client.forms import EnrollmentForm
 
@@ -18,4 +18,22 @@ class EnrollmentListView(TemplateView):
     def get_context_data(self, **kwargs) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context["enrollments"] = Enrollment.objects.all()
+
+        payments = []
+        for s in Student.objects.all():
+            enrollmets = Enrollment.objects.filter(student_id=s.id)
+            paid_total = 0
+            pending_total = 0
+            for e in enrollmets:
+                if e.status:
+                    paid_total += e.course_id.enrollment_fee
+                else:
+                    pending_total += e.course_id.enrollment_fee
+            data = {
+                "name": s.name,
+                "paid_total": paid_total,
+                "pending_total": pending_total,
+            }
+            payments.append(data)
+        context["payments"] = payments
         return context
