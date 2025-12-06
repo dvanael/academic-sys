680531c5f7f389d1df0047dbc6cdaa4124d86012
Ana Barbosa <anael.noki@gmail.com>
2025-12-05 23:33:13 -0300

    feat (client): adiciona dados simples para dashboard

--- client/views/index.py
+++ client/views/index.py
@@ -1,5 +1,15 @@
 from django.views.generic import TemplateView
+from data.models import Student, Course, Enrollment
+from typing import Any
 
 
 class IndexView(TemplateView):
     template_name = "index.html"
+
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["active_courses"] = Course.objects.filter(status=True)
+        context["count_students"] = Student.objects.count()
+        context["paid_enrollments"] = Enrollment.objects.filter(status=True).count()
+        context["pending_enrollments"] = Enrollment.objects.filter(status=False).count()
+        return context
