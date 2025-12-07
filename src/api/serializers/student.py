f55b6777a2f7d0e6fe0613fde11a1938668dab48
Ana Barbosa <anael.noki@gmail.com>
2025-12-06 23:35:30 -0300

    feat (api): modifica formato da data

--- src/api/serializers/student.py
+++ src/api/serializers/student.py
@@ -3,6 +3,8 @@ from data.models import Student
 
 
 class StudentSerializer(serializers.ModelSerializer):
+    admission_date = serializers.DateTimeField(format="%d/%m/%Y %H:%M")
+
     class Meta:
         model = Student
         fields = ["id", "name", "email", "cpf", "admission_date"]
